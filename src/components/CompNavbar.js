b9ce8f8c327398638edb238f82ce3b9a6690fda2
Aleksandar Stojanovic <sale4319@gmail.com>
2019-11-18 18:14:22 +0100

    Naaavbaaar

--- src/components/CompNavbar.js
+++ src/components/CompNavbar.js
@@ -56,7 +56,7 @@ export default function MenuAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Photos
+            Organizational Dashboard
           </Typography>
           {auth && (
             <div>
